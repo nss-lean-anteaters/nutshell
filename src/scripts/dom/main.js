b0fffd0f1a57cbf6ea10c647495c4bbff7bd44c3
Hayley Landsberg <hayleyheath@gmail.com>
2018-05-15 15:57:05 -0500

    Created API Manager and tested it in the main.js file

---
+++ src/scripts/dom/main.js
@@ -0,0 +1,32 @@
+const $ = require("jquery")
+const APIManager = require("../api/apiManager")
+
+// const userSection = $("user-section")
+console.log(APIManager.getAllObjects("user"))
+// const userResponse = APIManager.getAllObjects("user")
+// .then(response => userSection.append(response))
+
+const userList = function () {
+    // Get the animals
+    APIManager.getAllObjects("user")
+    .then(allUsers => {
+        allUsers.forEach(user => {
+        // Iterate over animal list
+        // user.forEach(response => {
+            // Build HTML representation for each one
+            //  ensure purchase button is on representation
+            console.log(user)
+            $("#user-section").append(
+            `
+                <div class="catalogItem" id="${user.id}">
+                    <section>
+                        ${user.user}
+                    </section>
+                </div>
+                `
+            )
+        })
+     })
+}
+
+userList()
