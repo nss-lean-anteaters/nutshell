66f4df279f670fb5c34a31cf8a1e2ad4148e5eec
David Paul <dfp333@gmail.com>
2018-05-16 12:52:34 -0500

    initial addition of verification, registration and login functions, still needs some work waiting on reg. page

---
+++ src/scripts/dom/user/adduser.js
@@ -0,0 +1,14 @@
+//accept input of username and email address, push to database with a unique id
+
+const $ = require("jquery")
+const APIManager = require("../api/apiManager")
+
+const registerUser = (username, email) => {
+    const users = APIManager.getAllObjects("user")
+    const userID = users.length + 1
+    users.push({
+        userID,
+        username,
+        email,
+    })
+}
