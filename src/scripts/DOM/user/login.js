609b8c8485d38a1a9da5759c1b8098089428063a
Deanna Vickers <deanna2000@gmail.com>
2018-05-16 15:55:39 -0500

    login user form

---
+++ src/scripts/DOM/user/login.js
@@ -0,0 +1,43 @@
+const apiManager = require("../../api/apiManager")
+const $ = require("jquery")
+const tagbuild = require("./../Utility/tagBuilder")
+const verify = require("./verify")
+
+// Start function for the login form to export
+const loginForm = function () {
+
+    // Button click opens the register form
+    $(document).ready(function() {
+        $("#loginID").on("click", function (event) {
+            console.log(event.currentTarget.id)
+            // Display order form for this animal
+            $("#user-section").empty()
+
+
+        // Create login/register form
+            tagbuild("div","user-section", "","", "form")
+            tagbuild("input", "form", "", "Please input your name", "username")
+            tagbuild("input", "form", "","Please input your email", "email")
+            tagbuild("button", "form", "Complete Login", "", "loginButton")
+
+            $("#loginButton").on("click", function (event) {
+                console.log(event.currentTarget.id)
+                let username = $("#username").val()
+                let email = $("#email").val()
+
+                console.log(username, email)
+                if (username === "" || email === "") {
+                    alert("Please fill all fields...!!!!!!")
+                    }else{
+                    // Call validation function here
+                    verify(username, email)
+                }
+
+            })
+        })
+    })
+}
+
+
+// Export the login form function
+module.exports = loginForm
--- src/scripts/dom/main.js
+++ src/scripts/dom/main.js
@@ -1,10 +1,16 @@
 const $ = require("jquery")
 const APIManager = require("../api/apiManager")
+const registerForm = require("./../DOM/user/register")
+const loginForm = require("./../DOM/user/login")
 
 // const userSection = $("user-section")
 console.log(APIManager.getAllObjects("user"))
 // const userResponse = APIManager.getAllObjects("user")
 // .then(response => userSection.append(response))
+
+registerForm()
+loginForm()
+
 
 const userList = function () {
     // Get the animals
