404decfb6c442bf1cd177ee8d6d31c50944c97d0
jcarter0149 <jacobraycarter@gmail.com>
2018-05-17 10:32:33 -0500

    added a rough display for messages

--- src/scripts/DOM/chat/showChat.js
+++ src/scripts/DOM/chat/showChat.js
@@ -12,11 +12,16 @@ const messageBase =
 document.createElement("section")
 messageBase.classList = "message-base"
 
+const messageInput = document.createElement("input")
+messageInput.setAttribute("type", "text")
+messageInput.classList = "messageInput"
+
 const sendBtn = document.createElement("button")
 sendBtn.classList = "sendBtn"
 sendBtn.textContent = "Send"
 
 messageOutput.append(messageCard)
 messageCard.append(messageBase)
+messageBase.append(messageInput)
 messageBase.append(sendBtn)
 
