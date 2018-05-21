9e6bba20fbef9f71fbb410f065261dd8032716fe
Deanna Vickers <deanna2000@gmail.com>
2018-05-21 11:14:49 -0500

    added logo

--- src/scripts/DOM/Utility/header.js
+++ src/scripts/DOM/Utility/header.js
@@ -1,7 +1,10 @@
 const $ = require("jquery")
 const tagbuild = require("./tagBuilder")
 
-
+tagbuild("img","nsHeader", "","","logoImage")
+const logo = document.querySelector("#logoImage")
+logo.setAttribute("src", "../../../IMAGES/anteater2.png")
+logo.setAttribute("height","200px")
 tagbuild("h3", "nsHeader", "Lean Anteaters Nutshell", "", "bannerTitle")
 tagbuild("p", "bannerTitle", "", "", "headerID")
 tagbuild("button", "headerID", "Register", "", "registerID")
